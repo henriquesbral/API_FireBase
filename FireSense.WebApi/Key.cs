1f1f06da1bc965852c08260008f96ed7d0b3c4e3
Carlos Henrique <74941109+henriquesbral@users.noreply.github.com>
2024-12-19 15:27:55 -0300

    Update Key.cs

--- FireSense.WebApi/Key.cs
+++ FireSense.WebApi/Key.cs
@@ -2,6 +2,6 @@
 {
     public class Key
     {
-        public static string Secret = "09622bbb133325d5f1fc111ccc168adf9489cc8b4b4f5548900aaea1be3171a6";
+        public static string Secret = "";
     }
 }
